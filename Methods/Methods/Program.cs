d5ffaa51ca3445bc536710952edfda3f9d3eea7d
onurcoskun1 <coskunonur01@gmail.com>
2022-04-19 22:46:38 +0300

    notes and practices about Methods in CSharp

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
